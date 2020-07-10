240a8a3574bb557f04274613c94ac120b195258d
Ailin Tobin <ailintobin@gmail.com>
2020-07-10 21:26:43 +0000

    created templates directory and allrecipes.html, installed flask-pymongo, and instatalled dns python

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipes'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')    
+def get_recipes():
+    return render_template("allrecipies.html", recipies=mongo.db.all_recipes.find())
+
 
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 5000)), debug=False)
