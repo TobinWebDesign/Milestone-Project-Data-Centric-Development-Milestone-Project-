09ef939f1ab50db8cc1792133538d9c609fb4053
Ailin Tobin <ailintobin@gmail.com>
2020-07-11 15:39:40 +0000

    amended app.py & allrecipes.html

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'recipes'
+'''app.config["MONGO_DBNAME"] = 'recipes'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")'''
 
 mongo = PyMongo(app)
 
@@ -17,5 +20,7 @@ def get_recipes():
     return render_template("allrecipes.html", recipes=mongo.db.all_recipes.find())
 
 
-
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 5000)), debug=False)
+if __name__ == "__main__":
+    app.run(host=os.getenv("IP", "0.0.0.0"),
+    port=int(os.getenv("PORT", 5000)),
+    debug=True)
