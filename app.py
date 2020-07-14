e9009929a7b4715e699c1adec86d50d93da253a5
Ailin Tobin <ailintobin@gmail.com>
2020-07-14 21:35:25 +0000

    edited add_recipe.html, added login.html

--- app.py
+++ app.py
@@ -23,6 +23,19 @@ def get_recipes():
     all_recipes = mongo.db.all_recipes.find()
     return render_template("allrecipes.html", all_recipes=all_recipes)
 
+'''Add recipe'''
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html',
+                           categories=mongo.db.categories.find(),
+                           meal_type=mongo.db.meal_type.find())
+
+'''login'''
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host=os.getenv("IP", "0.0.0.0"),
